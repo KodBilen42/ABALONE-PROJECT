7d3ea216097e24318e72ea9949e52684831fb94c
KodBilen42 <89701935+KodBilen42@users.noreply.github.com>
2021-08-29 19:30:37 +0300

    main function
    +command interpreter added
    +game is now playable through console

--- Abbalone Python/main.py
+++ Abbalone Python/main.py
@@ -1,10 +1,39 @@
 import board
 
-my_board = board.Board()
-my_board.balls = [[3, 4, "W"], [3, 5, "W"], [3, 6, "W"], [4, 4, "W"], [5, 4, "W"]]
 
-my_board.display()
+board = board.Board()
+board.initialize_board(default_board=True, show_indexes=True)
+board.display()
+selection = []
+while True:
+    command = input()
+    if command[:6] == "select":
+        data = command[-3:]
+        x = int(data[0])
+        y = int(data[1])
+        color = data[2]
+        selection.append([x, y, color])
+        print("ball selected")
 
-my_board.move([[3, 6, "W"], [3, 4, "W"], [3, 5, "W"]], 0)
+    elif command[:8] == "unselect":
+        data = command[-3:]
+        x = int(data[0])
+        y = int(data[1])
+        color = data[2]
+        if [x, y, color] in selection:
+            selection.remove([x, y, color])
+            print("ball unselected")
+        else:
+            print("wall wasn't selected")
 
-my_board.display()
+    elif command[:4] == "move":
+        direction = int(command[-1])
+        board.move(selection, direction)
+        selection = []
+        board.display()
+
+    elif command == "help":
+        pass
+
+    else:
+        print("invalid command")
