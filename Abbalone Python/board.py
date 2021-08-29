616592038ef3fcaa078b020262355afda168772e
KodBilen42 <89701935+KodBilen42@users.noreply.github.com>
2021-08-29 14:04:25 +0300

    Add files via upload

--- Abbalone Python/board.py
+++ Abbalone Python/board.py
@@ -223,13 +223,3 @@ class Board:
             moving_position.append(ball[2])
             self.balls.append(moving_position)
         return True
-
-
-my_board = Board()
-my_board.balls = [[3, 4, "W"], [3, 5, "W"], [3, 6, "W"], [4, 4, "W"], [5, 4, "W"]]
-
-my_board.display()
-
-my_board.move([[3, 6, "W"], [3, 4, "W"], [3, 5, "W"]], 0)
-
-my_board.display()
